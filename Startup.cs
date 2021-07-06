12b827b4d4f560ceefd14ec1cab8b68cc3f50970
dmitry-vychikov <diman896@gmail.com>
2021-07-06 16:28:22 +0300

    Elasticsearch confi

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Hangfire;
 using Hangfire.Dashboard;
@@ -11,6 +12,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace DSR_HangfireExample
 {
@@ -22,14 +26,34 @@ namespace DSR_HangfireExample
         {
             WebHostEnvironment = webHostEnvironment;
         }
+
+        private string DbHost => WebHostEnvironment.EnvironmentName == "Docker" ? "db" :"localhost";
+        private string ElasticSearchHost => WebHostEnvironment.EnvironmentName == "Docker" ? "elasticsearch" :"localhost";
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            DbConnectionStringBuilder builder = new()
+            services.AddLogging(builder =>
             {
-                ["Server"] = WebHostEnvironment.EnvironmentName == "Docker" ? "db" :"localhost",
+                var loggerConfiguration = new LoggerConfiguration()
+                    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri($"http://{ElasticSearchHost}:9200"))
+                    {
+                        AutoRegisterTemplate = true,
+                        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{DateTime.UtcNow:yyyy-MM}"
+                    });
+            
+                builder.AddSerilog(loggerConfiguration.CreateLogger());
+                builder.AddSimpleConsole(options =>
+                {
+                    options.IncludeScopes = true;
+                });
+            });
+            
+            DbConnectionStringBuilder connectionStringBuilder = new()
+            {
+                ["Server"] = DbHost,
                 ["Database"] ="hangfire",
                 ["User ID"] = "sa",
                 ["Password"] = "Your_password123"
@@ -39,7 +63,7 @@ namespace DSR_HangfireExample
             {
                 configuration
                     .UseSqlServerStorage(
-                        () => new Microsoft.Data.SqlClient.SqlConnection(builder.ConnectionString))
+                        () => new Microsoft.Data.SqlClient.SqlConnection(connectionStringBuilder.ConnectionString))
                     .UseSimpleAssemblyNameTypeSerializer()
                     ;
 
